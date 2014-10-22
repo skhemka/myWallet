7714a21223848f9682b3a81324f92dca0b6755b5
Shreeyagya Khemka <skhemka@cmu.edu>
2014-10-21 23:41:53 -0400

    -a

--- wallet/models.py
+++ wallet/models.py
@@ -8,28 +8,28 @@ class Wallet(models.Model):
     name = models.CharField(max_length=20)
     #creating a tuple of sizes
     SIZES = (
-        ('T', 'Tiny'),
-        ('N', 'Normal'),
-        ('H', 'Huge'),
+        ('Tiny', 'Tiny'),
+        ('Normal', 'Normal'),
+        ('Huge', 'Huge'),
         )
-    size = models.CharField(max_length=1, choices=SIZES)
+    size = models.CharField(max_length=10, choices=SIZES)
     #creating a tuple of colors
     COLORS = (
-        ('B', 'Black'),
-        ('G', 'Beige'),
-        ('W', 'White'),
-        ('R', 'Red')
+        ('Black', 'Black'),
+        ('Beige', 'Beige'),
+        ('White', 'White'),
+        ('Red', 'Red')
         )
-    color = models.CharField(max_length=1, choices=COLORS)
+    color = models.CharField(max_length=10, choices=COLORS)
     #creating tuple of card slots
     l1 = range(2,16)
     l2 = range(2,16)
     l = zip(l1,l2)
     card_slots = models.IntegerField(choices=l)
     #creating tuple for YES/NO
-    YN = [('Y', 'Yes'),('N','No')]
-    id_slot = models.CharField(max_length=1, choices=YN)
-    coin_pocket = models.CharField(max_length=1, choices=YN)
+    YN = [('Yes', 'Yes'),('No','No')]
+    id_slot = models.CharField(max_length=3, choices=YN)
+    coin_pocket = models.CharField(max_length=3, choices=YN)
     #creating tuple for money slots
     D = [(1,1),(2,2)]
     money_slots = models.SmallIntegerField(choices=D)
--- wallet/views.py
+++ wallet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404
 
 from wallet.models import Wallet
@@ -6,14 +6,10 @@ from wallet.models import Wallet
 # Create your views here.
 def index(request):
     wallet_styles = Wallet.objects.all()
-    print(wallet_styles)
     context = {'wallet_styles': wallet_styles}
     return render(request, 'wallet/index.html', context)
 
 def detail(request, style):
-    try:
-        wallet = Wallet.objects.get(pk=style)
-    except Wallet.DoesNotExist:
-        raise Http404
+    wallet = get_object_or_404(Wallet, pk=style)
     return render(request, 'wallet/detail.html', {'wallet': wallet})
-    
+
