80bb5787761d233b8a2c4caa1b8f016937bf940f
Shreeyagya Khemka <skhemka@cmu.edu>
2014-10-22 16:58:55 -0400

    Adding CSS Files

--- myWallet/settings.py
+++ myWallet/settings.py
@@ -80,5 +80,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+import os
+PROJECT_DIR=os.path.dirname(__file__)
+STATIC_ROOT= os.path.join(PROJECT_DIR,'static')
 
 STATIC_URL = '/static/'
